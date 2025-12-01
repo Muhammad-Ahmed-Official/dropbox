b078a11af25831ae16f775c2e3c47e9b66ccae52
Muhammad Ahmed <ahmedanis4546@gmail.com>
2025-12-01 17:16:53 +0500

    ..

--- src/sign-in/[[...sign-in]]/page.tsx
+++ src/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignInForm } from '@/components/SignInForm'
+// import { SignInForm } from '@/components/SignInForm'
 
 export default function page() {
   return (
     <section className="flex-1 flex items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      <SignInForm />
+      {/* <SignInForm /> */}
     </section>
 
   )

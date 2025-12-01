3cb8c8a1cdf31a1040c686d1658313e47f00bb88
Muhammad Ahmed <ahmedanis4546@gmail.com>
2025-12-01 16:22:09 +0500

    ..

--- src/sign-in/[[...sign-in]]/page.tsx
+++ src/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,11 @@
-import { SignUpForm } from '../../../components/SignUpForm'
+import { SignInForm } from '@/components/SignInForm'
+import React from 'react'
 
 export default function page() {
   return (
     <section className="flex-1 flex items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      <SignUpForm />
+      <SignInForm />
     </section>
+
   )
 }

088e336873e21513ca6236632ef35556edbe14f7
Muhammad Ahmed <ahmedanis4546@gmail.com>
2025-12-01 17:18:45 +0500

    ..

--- src/sign-up/[[...sign-up]]/page.tsx
+++ src/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUpForm } from '@/components/SignUpForm'
+// import { SignUpForm } from '@/components/SignUpForm'
 
 export default function page() {
   return (
     <section className="flex-1 flex items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      <SignUpForm />
+      {/* <SignUpForm /> */}
     </section>
   )
 }
